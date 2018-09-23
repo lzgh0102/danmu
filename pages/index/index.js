aadeb00bbd0fcd5931904f2386d809446b719155
zhaolei <zhaolei@yupaopao.cn>
2018-09-23 23:25:31 +0800

    base function

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,49 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+
+  onLoad: function(e) {
+    var textColor = "rgb(255, 255, 255)";
+    var speed = 10;
+    var text = "测试文字";
+    var thisPage = this;
+    wx.getSystemInfo({
+      success: function(res) {
+        var screenHeight = res.screenHeight;
+        var screenWidth = res.screenWidth;
+
+        // 定义文字移动的坐标
+        var xPosition = screenHeight;
+
+        var timer = setInterval(function() {
+
+          var textCanvas = wx.createCanvasContext('text-canvas');
+
+          // 旋转90度 让文字横屏
+          textCanvas.rotate(0.5 * Math.PI);
+
+          // 文字宽度 中间横条0.5倍屏幕宽
+          textCanvas.setFontSize(parseInt(screenWidth * 0.5));
+          // 获取文字长度
+          var textwidth = textCanvas.measureText(text).width;
+          textCanvas.setFillStyle(textColor);
+          textCanvas.setTextAlign("start");
+
+          textCanvas.save();
+          if (xPosition < -textwidth) {
+            xPosition = screenHeight;
+          }
+          textCanvas.fillText(text, xPosition--, -(0.25 * screenWidth));
+          textCanvas.draw();
+          textCanvas.restore();
+        }, speed);
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    });
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+
+  stopIt: function() {
+    clearInterval(timer);
+  },
+
+  onUnload: function() {
+    clearInterval(timer);
   }
 })
