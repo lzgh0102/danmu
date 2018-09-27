c3a7413c37c0e6795c32d26cb83ceab6f4475bd9
leizhao <leizhao0102@gmail.com>
2018-09-28 00:40:31 +0800

    add input

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,8 +1,8 @@
 var timer;
 Page({
   onLoad: function(e) {
-    var textColor = "rgb(255, 255, 255)";
-    var speed = 10;
+    var textColor = "rgb(255, 0, 0)";
+    var speed = 100;
     var text = "测试文字";
     var thisPage = this;
     wx.getSystemInfo({
@@ -21,17 +21,18 @@ Page({
           textCanvas.rotate(0.5 * Math.PI);
 
           // 文字宽度 中间横条0.5倍屏幕宽
-          textCanvas.setFontSize(parseInt(screenWidth * 0.5));
+          textCanvas.setFontSize(parseInt(screenWidth * 0.25));
           // 获取文字长度
           var textwidth = textCanvas.measureText(text).width;
           textCanvas.setFillStyle(textColor);
-          textCanvas.setTextAlign("start");
+          textCanvas.setTextBaseline('middle');
+          textCanvas.setTextAlign('start');
 
           textCanvas.save();
           if (xPosition < -textwidth) {
             xPosition = screenHeight;
           }
-          textCanvas.fillText(text, xPosition--, -(0.25 * screenWidth));
+          textCanvas.fillText(text, xPosition--, -(0.5 * screenWidth));
           textCanvas.draw();
           textCanvas.restore();
         }, speed);
