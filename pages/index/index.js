766774d82ce0b3a0b039e237bc58e9947c16479b
leizhao <leizhao0102@gmail.com>
2018-10-09 23:39:45 +0800

    base function

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,17 @@
 var timer;
 Page({
-  onLoad: function(e) {
+
+  data: {
+    showView: false,
+    text: ""
+  },
+
+  onLoad: function(options) {
+
+    showView: (options.showView == "true" ? true : false);
+
     var textColor = "rgb(255, 255, 255)";
-    var speed = 100;
-    var text = "测试文字";
+    var speed = 10;
     var thisPage = this;
     wx.getSystemInfo({
       success: function(res) {
@@ -15,13 +23,17 @@ Page({
 
         timer = setInterval(function() {
 
+          var text = thisPage.data.text;
+          if (!text) {
+            text = "手持弹幕";
+          }
           var textCanvas = wx.createCanvasContext('text-canvas');
 
           // 旋转90度 让文字横屏
           textCanvas.rotate(0.5 * Math.PI);
 
           // 文字宽度 中间横条0.5倍屏幕宽
-          textCanvas.setFontSize(parseInt(screenWidth * 0.25));
+          textCanvas.setFontSize(parseInt(screenWidth * 0.5));
           // 获取文字长度
           var textwidth = textCanvas.measureText(text).width;
           textCanvas.setFillStyle(textColor);
@@ -40,8 +52,35 @@ Page({
     });
   },
 
-  stopIt: function() {
-    clearInterval(timer);
+  // stopIt: function() {
+  //   clearInterval(timer);
+  // },
+
+  clickCanvas: function() {
+    var that = this;
+    that.setData({
+      showView: (!that.data.showView)
+    });
+
+    // clear input
+    // if (that.data.showView) {
+    //   that.setData({
+    //     text: ""
+    //   });
+    // }
+  },
+
+  getInput: function(e) {
+    var text = e.detail.value;
+    this.setData({
+      text: text
+    })
+  },
+
+  focusInput: function() {
+    this.setData({
+      text: ""
+    })
   },
 
   onUnload: function() {
