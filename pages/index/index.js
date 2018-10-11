7469750d9b9c2863c0b9e979b3d172d53e6da71b
leizhao <leizhao0102@gmail.com>
2018-10-11 23:51:27 +0800

    base function

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,15 +2,17 @@ var timer;
 Page({
 
   data: {
-    showView: false,
-    text: ""
+    showView: true,
+    text: "",
+    showModalStatus: false,
+    animationData: {},
+    currentColor: "white"
   },
 
   onLoad: function(options) {
 
     showView: (options.showView == "true" ? true : false);
 
-    var textColor = "rgb(255, 255, 255)";
     var speed = 5;
     var thisPage = this;
     wx.getSystemInfo({
@@ -22,8 +24,10 @@ Page({
         var xPosition = screenHeight;
 
         timer = setInterval(function() {
-
+          
           var text = thisPage.data.text;
+          var textColor = thisPage.data.currentColor;
+
           if (!text) {
             text = "手持弹幕";
           }
@@ -52,22 +56,26 @@ Page({
     });
   },
 
-  // stopIt: function() {
-  //   clearInterval(timer);
-  // },
-
   clickCanvas: function() {
     var that = this;
     that.setData({
       showView: (!that.data.showView)
     });
+    
+    that.hideModal();
+  },
 
-    // clear input
-    // if (that.data.showView) {
-    //   that.setData({
-    //     text: ""
-    //   });
-    // }
+  clickSetting: function() {
+    var that = this;
+    that.setData({
+      showModalStatus: true
+    });
+  },
+
+  onchangeColor: function(e) {
+    this.setData({
+      currentColor: e.target.id
+    });
   },
 
   getInput: function(e) {
@@ -85,5 +93,47 @@ Page({
 
   onUnload: function() {
     clearInterval(timer);
+  },
+
+  showModal: function() {
+    // 显示遮罩层
+    var animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "linear",
+      delay: 0
+    })
+    this.animation = animation
+    animation.translateY(300).step()
+    this.setData({
+      animationData: animation.export(),
+      showModalStatus: true
+    })
+    setTimeout(function() {
+      animation.translateY(0).step()
+      this.setData({
+        animationData: animation.export()
+      })
+    }.bind(this), 200)
+  },
+
+  hideModal: function() {
+    // 隐藏遮罩层
+    var animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "linear",
+      delay: 0
+    })
+    this.animation = animation
+    animation.translateY(300).step()
+    this.setData({
+      animationData: animation.export(),
+    })
+    setTimeout(function() {
+      animation.translateY(0).step()
+      this.setData({
+        animationData: animation.export(),
+        showModalStatus: false
+      })
+    }.bind(this), 200)
   }
 })
